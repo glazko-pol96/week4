04b1d22bf5dc7d791576f2a4e04b73668cf55c05
glazko-pol96 <57094984+glazko-pol96@users.noreply.github.com>
2021-12-01 14:24:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { Server } = require('http');
-const CORS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-  'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
-}
-const login = 'glazkopolina';
 
 const s = Server((req, res)=> {
+  const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+  }
+  const login = 'glazkopolina';
+
+  res.writeHead(200, { 'Content-Type': 'application/json', ...CORS});
+
   if (req.url === '/result4/') {
-    res.writeHead(200, { 'Content-Type': 'application/json', ...CORS});
     let body = [];
     req.on('data', (chunk) => {
       body.push(chunk);
     }).on('end', () => {
       body = Buffer.concat(body).toString();
+
       return res.end(JSON.stringify({
         message: author,
         'x-result': req.headers['x-test'],
@@ -21,7 +24,7 @@ const s = Server((req, res)=> {
       }))
     })
   } else {
-    return res.end(login)
+    res.end(login)
   }
 });
 s.listen(process.env.PORT)
